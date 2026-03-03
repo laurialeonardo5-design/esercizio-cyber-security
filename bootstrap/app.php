36ed91a758597556d85c8bbfcc313655ead4edd0
leonardo lauria <laurialeonardo5@gmail.com>
2026-03-03 17:34:55 +0100

    esercizio broken access controll finito

---
+++ bootstrap/app.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Middleware\AdminMiddleware;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withProviders([
+        App\Providers\FortifyServiceProvider::class,
+    ])
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            "admin"=>AdminMiddleware::class,
+            'auth' => \App\Http\Middleware\Authenticate::class,
+            'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
