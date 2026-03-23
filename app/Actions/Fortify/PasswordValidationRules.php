87f535520d903989407e44ec5ed0ae6cbc7654e2
leonardo lauria <laurialeonardo5@gmail.com>
2026-03-23 16:23:07 +0100

    aggiunte protezioni sul login

--- app/Actions/Fortify/PasswordValidationRules.php
+++ app/Actions/Fortify/PasswordValidationRules.php
@@ -13,6 +13,6 @@ trait PasswordValidationRules
      */
     protected function passwordRules(): array
     {
-        return ['required', 'string', Password::default(), 'confirmed'];
+        return ['required', 'string', Password::min(12)->letters()->symbols()->mixedCase(), 'confirmed'];
     }
 }
